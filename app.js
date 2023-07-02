7eaf3d293eaba90dac1258259212a2d79a7a2ee3
Phillip Long <phillipnlong@gmail.com>
2023-07-02 13:59:03 -0700

    edited home page

--- app.js
+++ app.js
@@ -1,7 +1,42 @@
+
+// ADJUSTING NAVBAR
+// ******************************************************
 const menu = document.querySelector("#mobile-menu");
 const menu_links = document.querySelector(".nav-menu");
-
 menu.addEventListener("click", function() {
     menu.classList.toggle("is-active");
     menu_links.classList.toggle("active");
 })
+// ******************************************************
+
+// SLIDESHOW
+// ******************************************************
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("my-slides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+// ******************************************************
