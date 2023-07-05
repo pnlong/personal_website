4b45d497c683f7ca17641e77afbc07ee5e8c94b3
Phillip Long <phillipnlong@gmail.com>
2023-07-04 17:59:22 -0700

    worked on adding card flip mechanics

--- projects.js
+++ projects.js
@@ -1,12 +1,10 @@
-// PROJECTS CARD FLIPS
-// ******************************************************
+// CODE FOR PROJECTS PAGE
 
-const cards = document.querySelectorAll(".card__inner");
+// flips the cards when they are clicked
+const cards = document.querySelectorAll(".project-inner");
 
 cards.forEach((card) => {
   card.addEventListener("click", function (e) {
     card.classList.toggle("is-flipped");
   });
 });
-
-// ******************************************************
